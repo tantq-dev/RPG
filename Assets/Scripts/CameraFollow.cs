2634a36d675a74396c3984b2606303cdc824b173
QT <tqut214@gmail.com>
2024-02-23 01:30:48 +0700

    separate code into PlayerController.cs and Mover.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    void Update()
+    void LateUpdate()
     {
         this.transform.position = target.transform.position;
     }
